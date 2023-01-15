0ed0988b046b4ed2612d4d81319fd7282b2ad550
Anir <anirtraning@gmail.com>
2023-01-15 14:28:26 +0100

    Update find_target.py

--- find_target.py
+++ find_target.py
@@ -1,14 +1,17 @@
 def find_target(target, array):
     results = []
-    i = 0
-    while i < len(array):
-        first = array[i]
-        for ele in array[i+1:]:
-            if (ele + first) == target:
-                results.append(array.index(first))
-                results.append(array.index(ele))
-        
-        i += 1
+    if len(array) == 2:
+        results = [0, 1]
+    else:
+        i = 0
+        while i < len(array):
+            first = array[i]
+            for ele in array[i+1:]:
+                if (ele + first) == target:
+                    results.append(array.index(first))
+                    results.append(array.index(ele))
+            
+            i += 1
     
     return results
     
@@ -16,3 +19,4 @@ def find_target(target, array):
     
 print(find_target(9, [2, 7,11,15])) # Result should == [0, 1]  
 print(find_target(6, [3,2,4])) # Result should == [1, 2]
+print(find_target(6, [3,3])) # Result should == [0, 1]
